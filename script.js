ab9938906d4ebc0a6a836917d8f6d204c3d4be02
Murilo-MRS <murilosantana1993@gmail.com>
2022-07-04 18:20:13 -0300

    add requisitos 8 corrigido

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
-const paletaCor = document.querySelector('#color-palette');
+const paletaCor = document.querySelectorAll('.color');
 const pixelBoard = document.querySelector('#pixel-board');
-const pixelColor = document.querySelectorAll('.color');
 
-function quadroPixel() {
-  for (let index = 0; index < 25; index += 1) {
-    const pixel = document.createElement('div');
-    pixel.classList = 'pixel white';
-    pixelBoard.appendChild(pixel);
-  }
+for (let index = 0; index < 25; index += 1) {
+  const pixel = document.createElement('div');
+  pixel.classList = 'pixel white';
+  pixelBoard.appendChild(pixel);
 }
-quadroPixel();
 
-for (let index = 0; index < pixelColor.length; index += 1) {
-  pixelColor[index].addEventListener('click', function (evento) {
+for (let index = 0; index < paletaCor.length; index += 1) {
+  paletaCor[index].addEventListener('click', function (evento) {
     const elementoCor = document.querySelector('.selected');
     elementoCor.classList.remove('selected');
     evento.target.classList.add('selected');
+  });
+}
+
+const pixelColor = document.querySelectorAll('.pixel');
+for (let index = 0; index < pixelColor.length; index += 1) {
+  pixelColor[index].addEventListener('click', function (e) {
+    const elementoCor = document.querySelector('.selected').classList[1];
+    e.target.className = 'pixel' + ' ' + elementoCor;
   });
 }
