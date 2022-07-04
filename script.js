da46452a33da2488ab58baddc42969ab5f72f7cb
Murilo-MRS <murilosantana1993@gmail.com>
2022-07-04 17:16:53 -0300

    add requisitos 7 corrigido

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
-const paletaCor = document.getElementById('color-palette');
-const pixelBoard = document.getElementById('pixel-board');
-
+const paletaCor = document.querySelector('#color-palette');
+const pixelBoard = document.querySelector('#pixel-board');
+const pixelColor = document.querySelectorAll('.color');
 
 function quadroPixel() {
-    for (let index = 0; index < 25; index += 1) {
-        const pixel = document.createElement('div');
-        pixel.classList = 'pixel white';
-        pixelBoard.appendChild(pixel)
-    }
+  for (let index = 0; index < 25; index += 1) {
+    const pixel = document.createElement('div');
+    pixel.classList = 'pixel white';
+    pixelBoard.appendChild(pixel);
+  }
 }
 quadroPixel();
+
+for (let index = 0; index < pixelColor.length; index += 1) {
+  pixelColor[index].addEventListener('click', function (evento) {
+    const elementoCor = document.querySelector('.selected');
+    elementoCor.classList.remove('selected');
+    evento.target.classList.add('selected');
+  });
+}
